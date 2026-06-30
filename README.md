b8c95d541006f36fc42f9a84954f6d2e37d849b4
THOMASterPRO <thomas@bezemer.net>
2026-07-01 00:07:30 +0200

    add initial implementation of locale files and README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # allergyTranslations
 Open-source locale files for the Allergy Flashcards app. This repository contains multilingual translations of food allergy and food preparation phrases, making it easier for developers to build safer, more accessible applications for international travelers.
+
+## Install
+
+```bash
+npm install allergy-translations
+```
+
+## Use
+
+Import a specific locale file:
+
+```js
+import en from "allergy-translations/locales/en.js";
+import fr from "allergy-translations/locales/fr.js";
+```
+
+Or import the bundled registry from the package root:
+
+```js
+import locales, { en, fr } from "allergy-translations";
+
+console.log(locales.en);
+```
---
+++ index.js
@@ -0,0 +1,80 @@
+import bg from "./locales/bg.js";
+import cz from "./locales/cz.js";
+import de from "./locales/de.js";
+import dk from "./locales/dk.js";
+import ee from "./locales/ee.js";
+import en from "./locales/en.js";
+import es from "./locales/es.js";
+import fi from "./locales/fi.js";
+import fr from "./locales/fr.js";
+import gr from "./locales/gr.js";
+import hr from "./locales/hr.js";
+import hu from "./locales/hu.js";
+import ie from "./locales/ie.js";
+import it from "./locales/it.js";
+import lt from "./locales/lt.js";
+import lv from "./locales/lv.js";
+import mt from "./locales/mt.js";
+import nl from "./locales/nl.js";
+import pl from "./locales/pl.js";
+import pt from "./locales/pt.js";
+import ro from "./locales/ro.js";
+import si from "./locales/si.js";
+import sk from "./locales/sk.js";
+import sv from "./locales/sv.js";
+
+export {
+  bg,
+  cz,
+  de,
+  dk,
+  ee,
+  en,
+  es,
+  fi,
+  fr,
+  gr,
+  hr,
+  hu,
+  ie,
+  it,
+  lt,
+  lv,
+  mt,
+  nl,
+  pl,
+  pt,
+  ro,
+  si,
+  sk,
+  sv,
+};
+
+export const locales = {
+  bg,
+  cz,
+  de,
+  dk,
+  ee,
+  en,
+  es,
+  fi,
+  fr,
+  gr,
+  hr,
+  hu,
+  ie,
+  it,
+  lt,
+  lv,
+  mt,
+  nl,
+  pl,
+  pt,
+  ro,
+  si,
+  sk,
+  sv,
+};
+
+export default locales;
